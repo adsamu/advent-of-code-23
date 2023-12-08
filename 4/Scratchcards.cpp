82518b7ce120f6a6192a95d957577087d9c731f1
adsamu <samuelson93@gmail.com>
2023-12-08 14:07:38 +0100

    done with day 4

--- 4/Scratchcards.cpp
+++ 4/Scratchcards.cpp
@@ -1,9 +1,13 @@
+#include <algorithm>
 #include <cmath>
+#include <cstddef>
 #include <iostream>
 #include <fstream>
+#include <map>
 #include <sstream>
 #include <vector>
 #include <set>
+#include <numeric>
 
 
 std::vector<std::string> split(const std::string& str, char delimiter) {
@@ -18,43 +22,62 @@ std::vector<std::string> split(const std::string& str, char delimiter) {
     	return result;
 }
 
-
-
-int getWinnings(std::string line){
+size_t getWinnings(std::string line){
 
 	std::vector<std::string> winVec{split(split(line, '|')[0], ' ')};
 	std::set<std::string> winningNumbers(winVec.begin(), winVec.end());
-
 	std::vector<std::string> myNumbers{split(split(line, '|')[1], ' ')};
 	
-	int wins{};
+	size_t wins{};
 	for (auto nr : myNumbers){
 		if (winningNumbers.contains(nr)){
 			if(!nr.empty()){
 
-				std::cout << nr << std::endl;
 				wins++;
+
 			}
 		}
 	}
 	return wins;
 }
 
+int scratchcards(std::vector<size_t>& cards, std::vector<size_t> const& winnings) {
+
+	std::vector<int> sums(cards.size(), 1);
+	for(auto card : cards) {
+		for (size_t copy = card+1; copy <= card+winnings[card]; copy++) {
+			if (copy < winnings.size()){
+
+				sums[copy] += sums[card];
+
+			}
+		}
+	}
+
+	return std::accumulate(sums.begin(), sums.end(), 0);
+}
+
 int main (int argc, char *argv[]) {
 	std::ifstream file;
 	std::string line;
-	file.open("input.txt");
+	std::vector<size_t> winnings;	
+	std::vector<size_t> cardNrs{};
+	size_t card{};
 	int sum{};
 
-	// int wins{getWinnings(split("Card 1: 41  48   83 86 17 | 83 86  6 31 17  9 48 53", ':')[1])};
-	// std::cout << wins << std::endl;
-	// sum += wins > 0 ? std::pow(2, wins - 1) : 0;
+	file.open("input.txt");
 	while (std::getline(file, line))
 	{	
-		int wins{getWinnings(split(line, ':')[1])};
+		size_t wins{getWinnings(split(line, ':')[1])};
+		winnings.push_back(wins);
 		sum += wins > 0 ? std::pow(2, wins - 1) : 0;
+		
+		cardNrs.push_back(card);
+		card++;
+
 	}
 	file.close();
-	std::cout << sum << std::endl;
+	std::cout << "Part one: " << sum << std::endl;
+	std::cout << "Part Two: "<< scratchcards(cardNrs, winnings) << std::endl;
 	return 0;
 }
